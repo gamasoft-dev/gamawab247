14f24642b896953bd795984f7018f5fb1a2e7b0e
chuzksy-codeactive <chuzksy@gmail.com>
2023-02-07 12:41:10 +0100

    Update transaction entity

--- src/services/BillProcessorAPI/Entities/BillTransaction.cs
+++ src/services/BillProcessorAPI/Entities/BillTransaction.cs
@@ -10,9 +10,14 @@ namespace BillProcessorAPI.Entities
         public ETransactionStatus Status { get; set; }
         public string SystemReference { get; set; }
         public string GatewayTransactionReference { get; set; }
-        public decimal Amount { get; set; }
+        public decimal AmountPaid { get; set; } // Include charges
+        public decimal BillAmount { get; set; }
+        public decimal PrinciPay { get; set; } // Amount without charge
         public decimal TransactionCharge { get; set; }
         public string Narration { get; set; }
+        public string Channel { get; set; }
+        public string ResourcePIN { get; set; } // PropertyPIN
+        public string ReceiptUrl { get; set; }
         public BillPayerInfo User { get; set; }
     }
 }
