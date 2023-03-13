3fd6e8405322051fb85d24e731b1ddb7d3281c6a
Ogbosuka Christopher <ogbosukachris@gmail.com>
2023-03-13 11:57:29 +0100

    Request and Complaint Implementation
    Request and Complaint Implementation

---
+++ src/Libraries/Application/DTOs/RequestAndComplaintDtos.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Domain.Entities.Identities;
+using Domain.Enums;
+using System.Collections.Generic;
+
+namespace Application.DTOs
+{
+	public class RequestAndComplaintDto
+	{
+        /// <summary>
+        /// This is either user whatsapp username or phonenumber, instgram username etc
+        /// </summary>
+        public string CustomerId { get; set; }
+
+        public string Subject { get; set; }
+
+        public string Channel { get; set; }
+
+        public string Detail { get; set; }
+
+        public List<string> Responses { get; set; }
+
+        /// <summary>
+        /// This is the complaint or request unique identifier. This is generated for every ticker raised
+        /// </summary>
+        public string TicketId { get; set; }
+
+        public Guid BusinessId { get; set; }
+
+        public ERequestComplaintType Type { get; set; }
+
+        public string CallBackUrl { get; set; }
+
+        public DateTime? ResolutionDate { get; set; }
+
+        public string ResolutionStatus { get; set; }
+
+        public Guid? TreatedById { get; set; }
+
+        public string TreatedBy { get; set; }
+
+    }
+
+    public class CreateRequestAndComplaintDto {
+
+        public CreateRequestAndComplaintDto()
+        {
+            ResolutionStatus = EResolutionStatus.Pending.ToString();
+        }
+
+        /// <summary>
+        /// This is either user whatsapp username or phonenumber, instgram username etc
+        /// </summary>
+        public string CustomerId { get; set; }
+
+        public string Subject { get; set; }
+
+        public string Channel { get; set; }
+
+        public string Detail { get; set; }
+
+        /// <summary>
+        /// This is the complaint or request unique identifier. This is generated for every ticker raised
+        /// </summary>
+        internal string TicketId { get; set; }
+
+        public Guid BusinessId { get; set; }
+
+        public ERequestComplaintType Type { get; set; }
+
+        public string CallBackUrl { get; set; }
+
+        public string ResolutionStatus { get; set; }
+
+        public Guid? TreatedById { get; set; }
+
+    }
+
+    public class UpdateRequestAndComplaintDto: CreateRequestAndComplaintDto
+    {
+        public string Response { get; set; }
+    }
+}
+
