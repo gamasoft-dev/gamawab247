6038a5f2220e84fe22799479f75a1c3e860f1273
Snipercadet <donibris@gmail.com>
2023-05-09 09:08:14 +0100

    BroadcastMessage entity config

---
+++ src/Libraries/Domian/Entities/BroadcastMessage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Common;
+using Domain.Enums;
+
+namespace Domain.Entities
+{
+    public class BroadcastMessage : AuditableEntity
+    {
+        public Guid Id { get; set; }
+        public string  To { get; set; }
+        public string From { get; set; }
+        public string ApiKey { get; set; }
+        public string Message { get; set; }
+        public EBroadcastMessageStatus  Status { get; set; }
+        public Guid BusinessId { get; set; }
+        public Business Business { get; set; }
+    }
+}
