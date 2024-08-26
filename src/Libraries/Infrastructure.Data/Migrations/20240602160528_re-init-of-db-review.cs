c30d1d4a06be6826e64dbdec14b8c760071e5b98
Ogbosuka Christopher <ogbosukachris@gmail.com>
2024-08-26 08:57:58 +0100

    update

---
+++ src/Libraries/Infrastructure.Data/Migrations/20240602160528_re-init-of-db-review.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Data.Migrations
+{
+    public partial class reinitofdbreview : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "EmailAddress",
+                table: "BroadcastMessages",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FullName",
+                table: "BroadcastMessages",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EmailAddress",
+                table: "BroadcastMessages");
+
+            migrationBuilder.DropColumn(
+                name: "FullName",
+                table: "BroadcastMessages");
+        }
+    }
+}
